056e3367d40763d335b71836d3299c9b7084a5d8
Bence Zambo <zambo.bence@indotek.hu>
2026-02-24 12:46:23 +0100

    added github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/test-gh-actions-fe",
+  base: "/test-gh-actions-fe/",
+  test: {
+    globals: true,
+    environment: "jsdom", // important for React testing
+  },
 });
